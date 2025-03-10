1e987d32aea439b16e69e3afb6b9e48337622614
zaidsaadedin1 <zaidsaadedin11@gmail.com>
2025-03-10 11:12:31 +0300

    stage 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 // next.config.mjs
-import pkg from "./next-i18next.config.js"; // Import the entire module
-const { i18n } = pkg; // Destructure `i18n` from the imported module
+// import pkg from "./next-i18next.config.js"; // Import the entire module
+// const { i18n } = pkg; // Destructure `i18n` from the imported module
 
 const nextConfig = {
-  i18n,
+  i18n: {
+    locales: ["en", "ar"],
+    defaultLocale: "en",
+    localeDetection: false,
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
