586e9b9f7bf862db641a8a726b9361fcb5c6c9db
Zaidsaadedin1 <zaidsaadedin11@gmail.com>
2025-07-08 00:00:05 +0300

    lint fix

--- app/components/Ui/TiltedCard/TiltedCard.tsx
+++ app/components/Ui/TiltedCard/TiltedCard.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import type { SpringOptions } from "framer-motion";
 import { useRef, useState } from "react";
 import { motion, useMotionValue, useSpring } from "framer-motion";
